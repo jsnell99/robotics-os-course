ea502f1801950bb9e34bb409708a3f24fb847200
Jeffrey Snell <jeffrey_snell@student.uml.edu>
2022-02-14 11:01:03 -0500

    updated hw4 files

--- packages/hw2_package/src/hw3_pubsub.py
+++ packages/hw2_package/src/hw3_pubsub.py
@@ -8,19 +8,28 @@ import rospy
 
 class pubsubber:
 	def __init__(self):
-		#self.pub_msg = UnitsLabelled()
-		#self.pub_msg.units = 'feet'
 		rospy.Subscriber("/mystery/output2", UnitsLabelled, self.callback)			#register subscriber with ROS
 		self.pub = rospy.Publisher('hw3_unit_converted', UnitsLabelled, queue_size=10)	#register publisher with ROS
 		
 	def callback(self, msg):
 		rospy.loginfo(rospy.get_caller_id() + " published {}".format(msg))
+		#if rospy.has_param("meters"):
+		#	self.foo=rospy.get_param("meters")
+		#	self.publish(msg)
+		#if rospy.has_param("feet"):
+			#self.foo=rospy.get_param("feet")
 		msg.value = msg.value*3.28084
-		
 		msg.units='feet'
-		#self.pub_msg.value = msg.value*3.28084
 		rospy.loginfo(msg)
 		self.pub.publish(msg)	#publish message
+		#if rospy.has_param("smoots"):
+		#	self.foo=rospy.get_param("smoots")
+		#	msg.value = msg.value/1.7018
+		#	msg.units = 'smoots'
+		#	rospy.loginfo(msg)
+		#	self.pub.publish(msg)
+		#else:
+		#	self.foo="default"
 		
 if __name__ == '__main__':
 	rospy.init_node('hw3_pubsub', anonymous=True)

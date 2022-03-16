f9f771eb48ad557cec2acbad5cc6288b081bedc7
Jeffrey Snell <jeffrey_snell@student.uml.edu>
2022-03-16 12:45:45 -0400

    adding hw6 files

--- packages/odometry/src/hw6.py
+++ packages/odometry/src/hw6.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
 
 import rospy
-from odometry_hw_msgs.msg import (DistWheel, Pose2D)
+from odometry_hw.msg import (DistWheel, Pose2D)
 from math import sin, cos
 
 class pubsubber:
-	theta = 0
-	x = 0
-	y = 0
 	def __init__(self):
 		rospy.Subscriber("/dist_wheel", DistWheel, self.callback)	#register subscriber with ROS
 		self.pub = rospy.Publisher('/pose', Pose2D, queue_size=10)	#register publisher with ROS
 		
+		self.pose = Pose2D()
+		self.theta = 0
+		self.x = 0
+		self.y= 0
+
 	def callback(self, msg):
 		rospy.loginfo(rospy.get_caller_id() + " published {}".format(msg))
 		rospy.loginfo(msg)
@@ -20,20 +22,24 @@ class pubsubber:
 		dist_left = msg.dist_wheel_left
 		dist_right = msg.dist_wheel_right
 		dist_mid = (dist_left+dist_right)/2
-		delta_theta = (dist_r-dist_l)/(2*L)
-		delta_x = delta__mid*cos(theta+delta_theta/2)
-		delta_y = delta__mid*sin(theta+delta_theta/2)
+		delta_theta = (dist_right-dist_left)/(2*L)
+		delta_x = dist_mid*cos(self.theta+delta_theta/2)
+		delta_y = dist_mid*sin(self.theta+delta_theta/2)
 
-		theta+=delta_theta
-		x+= delta_x
-		y+= delta_y
+		
+		self.theta += delta_theta
+		self.x += delta_x
+		self.y += delta_y
+		#last_theta = 
 
-		new_coords = [x, y, theta]
-		self.pub.publish(new_coords)	#publish message
+		self.pose.x = self.x
+		self.pose.y = self.y
+		self.pose.theta = self.theta
+		self.pub.publish(self.pose)	#publish message
 
 		
 if __name__ == '__main__':
-	rospy.init_node('hw3_pubsub', anonymous=True)
+	rospy.init_node('pos_tracker', anonymous=True)
 	pubsubber()
 	rospy.spin() # spin() simply keeps python from exiting until this node is stopped
 	
